84d2ef70389940bd260c6492a50b270af12082e9
Luis Enrique Machado Urbalejo <luis.machado16@tectijuana.edu.mx>
2021-01-08 15:35:09 -0800

    Practica para tópicos
    Codigo de un proyecto

---
+++ Practica AlgoritmoHashing/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace Practica_AlgoritmoHashing
+{
+    partial class FrmInicio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmInicio));
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.txtIngresar = new System.Windows.Forms.TextBox();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.Tabla = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.lineShape1 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.Cprogreso = new CircularProgressBar.CircularProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Tabla)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(260, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 17);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Ingresa la Clave:";
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.BackColor = System.Drawing.Color.Transparent;
+            this.btnInsertar.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.btnInsertar.FlatAppearance.BorderSize = 2;
+            this.btnInsertar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnInsertar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DimGray;
+            this.btnInsertar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInsertar.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)), true);
+            this.btnInsertar.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.btnInsertar.Location = new System.Drawing.Point(264, 54);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(271, 44);
+            this.btnInsertar.TabIndex = 19;
+            this.btnInsertar.Text = "Insertar  /  Buscar";
+            this.btnInsertar.UseVisualStyleBackColor = false;
+            this.btnInsertar.Click += new System.EventHandler(this.btnInsertar_Click);
+            // 
+            // txtIngresar
+            // 
+            this.txtIngresar.BackColor = System.Drawing.SystemColors.WindowText;
+            this.txtIngresar.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtIngresar.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtIngresar.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIngresar.ForeColor = System.Drawing.SystemColors.Window;
+            this.txtIngresar.Location = new System.Drawing.Point(263, 32);
+            this.txtIngresar.Name = "txtIngresar";
+            this.txtIngresar.Size = new System.Drawing.Size(254, 18);
+            this.txtIngresar.TabIndex = 18;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.Color.Transparent;
+            this.btnSalir.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnSalir.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.btnSalir.FlatAppearance.BorderSize = 2;
+            this.btnSalir.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnSalir.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DimGray;
+            this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalir.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)), true);
+            this.btnSalir.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.btnSalir.Location = new System.Drawing.Point(264, 104);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(271, 44);
+            this.btnSalir.TabIndex = 21;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // Tabla
+            // 
+            this.Tabla.AllowUserToOrderColumns = true;
+            this.Tabla.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.Tabla.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.Tabla.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Tabla.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Tabla.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleVertical;
+            this.Tabla.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.Tabla.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.Tabla.ColumnHeadersHeight = 25;
+            this.Tabla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.Tabla.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.Tabla.EnableHeadersVisualStyles = false;
+            this.Tabla.GridColor = System.Drawing.Color.Red;
+            this.Tabla.Location = new System.Drawing.Point(12, 12);
+            this.Tabla.Name = "Tabla";
+            this.Tabla.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.ControlDarkDark;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.Tabla.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.Tabla.RowHeadersVisible = false;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.Snow;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.ControlDarkDark;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.White;
+            this.Tabla.RowsDefaultCellStyle = dataGridViewCellStyle3;
+            this.Tabla.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.Tabla.Size = new System.Drawing.Size(246, 328);
+            this.Tabla.TabIndex = 23;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "P.Memoria";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 93;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Clave";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 65;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Colicion";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 86;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(228, 93);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(0, 14);
+            this.linkLabel1.TabIndex = 24;
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.lineShape1});
+            this.shapeContainer1.Size = new System.Drawing.Size(540, 346);
+            this.shapeContainer1.TabIndex = 25;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // lineShape1
+            // 
+            this.lineShape1.BorderColor = System.Drawing.SystemColors.ControlLight;
+            this.lineShape1.Name = "lineShape1";
+            this.lineShape1.X1 = 263;
+            this.lineShape1.X2 = 516;
+            this.lineShape1.Y1 = 51;
+            this.lineShape1.Y2 = 51;
+            // 
+            // Cprogreso
+            // 
+            this.Cprogreso.AnimationFunction = WinFormAnimation.KnownAnimationFunctions.Liner;
+            this.Cprogreso.AnimationSpeed = 5000;
+            this.Cprogreso.BackColor = System.Drawing.Color.Transparent;
+            this.Cprogreso.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold);
+            this.Cprogreso.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Cprogreso.InnerColor = System.Drawing.Color.Transparent;
+            this.Cprogreso.InnerMargin = 2;
+            this.Cprogreso.InnerWidth = -1;
+            this.Cprogreso.Location = new System.Drawing.Point(263, 148);
+            this.Cprogreso.MarqueeAnimationSpeed = 2000;
+            this.Cprogreso.Name = "Cprogreso";
+            this.Cprogreso.OuterColor = System.Drawing.Color.Black;
+            this.Cprogreso.OuterMargin = -25;
+            this.Cprogreso.OuterWidth = 26;
+            this.Cprogreso.ProgressColor = System.Drawing.Color.Red;
+            this.Cprogreso.ProgressWidth = 25;
+            this.Cprogreso.SecondaryFont = new System.Drawing.Font("Imprint MT Shadow", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cprogreso.Size = new System.Drawing.Size(277, 198);
+            this.Cprogreso.StartAngle = 270;
+            this.Cprogreso.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.Cprogreso.SubscriptColor = System.Drawing.Color.Red;
+            this.Cprogreso.SubscriptMargin = new System.Windows.Forms.Padding(10, -35, 0, 0);
+            this.Cprogreso.SubscriptText = ".23";
+            this.Cprogreso.SuperscriptColor = System.Drawing.Color.Red;
+            this.Cprogreso.SuperscriptMargin = new System.Windows.Forms.Padding(10, 35, 0, 0);
+            this.Cprogreso.SuperscriptText = "°C";
+            this.Cprogreso.TabIndex = 30;
+            this.Cprogreso.TextMargin = new System.Windows.Forms.Padding(5, 8, 0, 0);
+            this.Cprogreso.Value = 68;
+            this.Cprogreso.Click += new System.EventHandler(this.Cprogreso_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(315, 232);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(186, 33);
+            this.label2.TabIndex = 31;
+            this.label2.Text = "Procesando";
+            // 
+            // FrmInicio
+            // 
+            this.AcceptButton = this.btnInsertar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.CancelButton = this.btnSalir;
+            this.ClientSize = new System.Drawing.Size(540, 346);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Cprogreso);
+            this.Controls.Add(this.btnInsertar);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtIngresar);
+            this.Controls.Add(this.Tabla);
+            this.Controls.Add(this.shapeContainer1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmInicio";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Practica -- Algoritmo de hashing";
+            this.Load += new System.EventHandler(this.FrmInicio_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Tabla)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnInsertar;
+        private System.Windows.Forms.TextBox txtIngresar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.DataGridView Tabla;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape1;
+        private CircularProgressBar.CircularProgressBar Cprogreso;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ Practica AlgoritmoHashing/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Dynamic;
+using System.Diagnostics;
+
+namespace Practica_AlgoritmoHashing
+{
+    public partial class FrmInicio : Form
+    {
+        public FrmInicio()
+        {
+            InitializeComponent();
+            SetStyle(ControlStyles.SupportsTransparentBackColor,
+                     true);
+        }
+
+        public Indice[] Tablas = new Indice[250];
+        public String[] Alfabeto = new String[26];
+
+
+        public void LLenarAlfabeto()
+        {
+            int c = 1;
+
+            char inicio = 'A';
+
+            Alfabeto[0] = inicio.ToString();
+
+            while (inicio < 'Z')
+            {
+                inicio++;
+                Alfabeto[c] = inicio.ToString();
+                c++;
+
+            }
+
+            for (int x = 0; x < Tablas.Length; x++)
+            {
+                Tablas[x] = new Indice();
+                Tablas[x].Memoria = x;
+            }
+        }      
+        private void FrmInicio_Load(object sender, EventArgs e)
+        {
+            LLenarAlfabeto();
+            //solamente sirve para diseño
+            Cprogreso.Value += 1;
+            if (Cprogreso.Value == 100)
+            {
+                Cprogreso.Value = 0;
+            }
+            else {
+                if (Cprogreso.Value == 0)
+                {
+                    Cprogreso.Value +=1;
+               }
+            }
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            Ingresando_Busqueda(txtIngresar.Text);
+            LLenarTabla();
+        }
+
+        public void LLenarTabla()
+        {
+
+            Tabla.Rows.Clear();
+
+            for (int x = 1; x < Tablas.Length; x++)
+            {
+
+                if (Tablas[x].Colicion == 221)
+                {
+                    Tabla.Rows.Add(Tablas[x].Memoria, Tablas[x].ClaveBusqueda, "");
+                }
+                else
+                {
+                    Tabla.Rows.Add(Tablas[x].Memoria, Tablas[x].ClaveBusqueda, Tablas[x].Colicion);
+                }
+            }
+        }
+        //Aqui es de buscar la letras en alfabeticamento.
+        public int Buscar(String letra)
+        {
+            int salida = 0;
+
+            for (int x = 0; x < Alfabeto.Length; x++)
+            {
+
+                if (Alfabeto[x] == letra)
+                {
+                    salida = x;
+                    break;
+                }
+            }
+            return salida;
+        }
+
+        //aqui inicia la busqueda, pero primero transforma las letras String a un numero int.
+        public void Ingresando_Busqueda(String ClaveBusqueda)
+        {
+            int Clave = 1;
+
+            foreach (char letras in ClaveBusqueda)
+            {
+                Clave += Buscar(letras.ToString());
+            }
+
+        //Buscar la Clave
+            if (Tablas[Clave].ClaveBusqueda == "")
+            {
+                Tablas[Clave].ClaveBusqueda = ClaveBusqueda;
+                MessageBox.Show("Fue Agregada en la Posicion " + Clave.ToString());
+            }
+            else
+            {
+                if (Tablas[Clave].ClaveBusqueda != ClaveBusqueda)
+                {
+                    if (Tablas[Clave].Colicion != 221)
+                    {
+                        int C = Tablas[Clave].Colicion;
+                    }
+                    else
+                    {
+                        NuevaColicion(Clave, ClaveBusqueda);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Se encontro en la Posicion " + Clave.ToString());
+                }
+            }
+
+        }
+        //Cuando pasa una nueva colision.
+        public void NuevaColicion(int Clave, String ClaveBusqueda)
+        {
+
+            for (int x = 249; x > 0; x--)
+            {
+                if (Tablas[x].ClaveBusqueda == "")
+                {
+                    Tablas[Clave].Colicion = x;
+                    Tablas[x].ClaveBusqueda = ClaveBusqueda;
+                    MessageBox.Show("Fue Agregada en la Posicion " + x.ToString());
+                    break;
+                }
+            }
+        }
+        public class Indice
+        {
+            public int Memoria = 221;
+            public String ClaveBusqueda = "";
+            public int Colicion = 221;
+
+            public Indice(int m, String cb, int c)
+            {
+                Memoria = m;
+                cb = ClaveBusqueda;
+                c = Colicion;
+            }
+
+            public Indice()
+            {
+            }
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        { 
+            label2.Text = "   Cerrando";
+                Close();
+           
+        }
+
+        private void Cprogreso_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnbuscar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
